a348bed2d311884496d2c43e35380c7faf778a63
Eva Leygonie <eva.leygonie@gmail.com>
2025-02-18 20:22:46 +0100

    Added Jons chat code for inspo

---
+++ src/Inspiration/Message.vue
@@ -0,0 +1,28 @@
+<template>
+   <li
+      @click="like(message)"
+      :style="{ alignSelf: message.self ? 'end' : 'start' }"
+    >
+      {{ message.text }}
+      <img
+        alt=""
+        :src="message.image"
+        v-show="message.image !== undefined"
+      />
+      <span class="reaction">{{ message.reaction }}</span>
+      <span class="time">
+        <span v-if="message.delivered">✓</span>
+        <span v-else>⨯</span>
+      </span>
+    </li>
+</template>
+
+<script>
+export default {
+  props: {
+    message: Object,
+  },
+}
+</script>
+
+<style></style>
