e807636be36f293507fa39b66e5c702907aca123
josefineluther <josefineluther@hotmail.se>
2025-02-18 13:26:20 +0100

    lite ändringar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
-<script></script>
+<script setup>
+  import SearchBar from '../components/SearchBar.vue'
+</script>
 
 <template>
+  <SearchBar />
   <p>Annonssida utan detaljer tills man loggat in</p>
 </template>
