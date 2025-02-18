ac3dba13cb517b6a779533abd36753d1d8700d64
josefineluther <josefineluther@hotmail.se>
2025-02-18 13:10:36 +0100

    Create SearchBar.vue

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <input type="text" placeholder="Sök...">
+    <input type="submit" value="Search">
+    <h3>Suggestions:</h3>
+    <input type="submit" value="Learn Spanish">
+    <input type="submit" value="Learn French">
+    <input type="submit" value="Learn Italian">
+    <input type="submit" value="Learn German">
+    <input type="submit" value="Learn Mandarin">
+</template>
+
+<style>
+
+</style>
