d9de3e68fecadc2cef516e68fbb5afc47a2ab14a
Eva Leygonie <eva.leygonie@gmail.com>
2025-02-18 11:04:34 +0100

    Main css kopplad + favicon

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
+import '../assets/main.css'
+
 import { createApp } from "vue";
 
+import App from './App.vue'
+import router from './router.js'
 
-import App from "./App.vue";
-import router from "./router"
-
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).mount('#app')
