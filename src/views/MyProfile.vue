70bf00e5ff09547cf04b8bb124735198ecb0d5a7
Therese Lagerhäll <tes_san_89@hotmail.com>
2025-02-21 19:11:17 +0100

    added some css

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -18,8 +18,11 @@ export default {
         class="ProfileImage"
       />
     </div>
-
-    <p>
+    <h1>Interests</h1>
+    <p class="ProfileText">
+      I speak <span>Swedish</span>. I want to learn <span>Italian</span>
+    </p>
+    <p class="ProfileText">
       Hi! I would love to learn Italian before my trip to Sicily this summer.
     </p>
     <p></p>
@@ -30,3 +33,57 @@ export default {
     </ul>
   </main>
 </template>
+
+<style>
+span {
+  text-decoration: underline;
+}
+.FriendAvatarImage {
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  margin: 30px;
+}
+
+.ProfileImage {
+  width: 150px;
+  height: 150px;
+  display: flex;
+  justify-content: center;
+
+  border-radius: 60%;
+  object-fit: cover;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.MainContainer {
+  margin-top: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+}
+
+.ProfileText {
+  margin: 10px;
+}
+
+.interest-container {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.InterestTag {
+  margin: 5px;
+  padding: 10px;
+  background-color: #fa812f;
+  color: #fef3e2;
+  border-radius: 5px;
+  font-weight: bold;
+  display: inline-block;
+}
+</style>
