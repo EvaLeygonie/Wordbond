ca2e8f31d03999a20e6fe2694bac86583eee4467
Therese Lagerhäll <tes_san_89@hotmail.com>
2025-02-26 16:36:20 +0100

    added new file editprofile.vue

--- src/components/EditProfil.vue
+++ src/components/EditProfil.vue
@@ -1,6 +1,18 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import axios from "axios";
-
-const profile = ref({ name: "", email: "", bio: "" });
+import { ref } from "vue";
 </script>
+
+<template>
+  <div class="EditProfile">
+    <h2></h2>
+    <form @submit.prevent="savePrrofile">
+      <label for="username">Username</label>
+      <input id="username" v-model="profile.username" type="text" required />
+
+      <label for="email">Email</label>
+      <input id="email" v-model="profile.email" type="email" required />
+      <label for="bio">Biografy</label
+      ><textarea id="bio" v-model="profile.bio"></textarea>
+    </form>
+  </div>
+</template>
