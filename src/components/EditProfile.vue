d3bb0e5b7703d69e14671a7c61c7d596366118d3
Therese Lagerhäll <tes_san_89@hotmail.com>
2025-02-27 15:14:18 +0100

    Update EditProfile.vue

--- src/components/EditProfile.vue
+++ src/components/EditProfile.vue
@@ -1,7 +1,8 @@
 <script setup>
   import { ref } from 'vue'
 
-  const name = ref('LinguaLover')
+  const username = ref('LinguaLover')
+  const password = ref('IloveBooks')
   const learningLanguage = ref('Swedish')
   const teachingLanguage = ref('Spanish')
   const profileText = ref(
@@ -10,7 +11,7 @@
 
   const saveProfile = () => {
     alert('Profile saved', {
-      name: name.value,
+      username: usernamename.value,
       teachingLanguage: teachingLanguage.value,
       learningLanguage: learningLanguage.value,
       profileText: profileText.value
@@ -23,7 +24,13 @@
     <h1>Edit profile</h1>
 
     <label for="username">Username</label>
-    <input id="username" v-model="name" type="text" />
+
+    <input id="username" v-model="username" type="text" />
+    <label for="password" />password<input
+      id="password"
+      v-model="password"
+      type="password"
+    />
 
     <label for="teachingLanguage">Language I can teach</label>
     <input id="teachingLanguage" v-model="teachingLanguage" type="text" />
@@ -31,13 +38,15 @@
     <label for="learningLanguage">Language I want to learn</label>
     <input id="learningLanguage" v-model="learningLanguage" type="text" />
 
-    <label for="profileText">Profile Text</label>
+    <label for="profileText">Biograph</label>
     <textarea id="profileText" v-model="profileText" />
 
-    <button @click="saveProfile">Save</button>
-    <RouterLink to="/myprofile"
-      ><button class="edit-button">Cancel</button></RouterLink
-    >
+    <div class="buttonContainer">
+      <button @click="saveProfile">Save</button>
+      <RouterLink to="/myprofile"
+        ><button class="edit-button">Cancel</button></RouterLink
+      >
+    </div>
   </div>
 </template>
 
@@ -48,5 +57,26 @@
     align-items: center;
     flex-direction: column;
     margin-top: 50px;
+  }
+  .buttonContainer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+  textarea {
+    width: 200px;
+    height: 150px;
+  }
+  button {
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 4px 16px rgba(0, 0, 0, 0.2);
+    margin: 5px;
+    padding: 5px;
+    background-color: #fa812f;
+    color: #fef3e2;
+    border-radius: 5px;
+    font-weight: bold;
+    display: inline-block;
+    border-color: #fa812f;
   }
 </style>
