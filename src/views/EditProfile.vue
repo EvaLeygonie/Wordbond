a373bc995fc69d8479f30a9644f0e42c3152c7be
Therese Lagerhäll <tes_san_89@hotmail.com>
2025-03-10 11:54:09 +0100

    added placeholder to edit profile

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -18,22 +18,37 @@
 
 <template>
   <div class="EditProfile">
-    <h2>Redigera profil</h2>
+    <h2>Edit profile</h2>
     <form @submit.prevent="saveChanges">
       <label>Username:</label>
-      <input v-model="editedUser.name" type="text" />
+      <input placeholder="Name" v-model="editedUser.name" type="text" />
 
       <label>Password:</label>
-      <input v-model="editedUser.password" type="password" />
+      <input
+        placeholder="Password"
+        v-model="editedUser.password"
+        type="password"
+      />
       <label>Age</label>
-      <input v-model="editedUser.age" type="text" />
+      <input placeholder="Age" v-model="editedUser.age" type="text" />
 
       <label>Biography:</label>
-      <textarea v-model="editedUser.bio" />
+      <textarea
+        placeholder="Tell me something about yourself"
+        v-model="editedUser.bio"
+      />
       <label>Language to learn</label>
-      <input v-model="editedUser.languageToLearn" type="text" />
+      <input
+        placeholder="Language you want to learn"
+        v-model="editedUser.languageToLearn"
+        type="text"
+      />
       <label>Language to teach</label>
-      <input v-model="editedUser.languageToTeach" type="text" />
+      <input
+        placeholder="Language you can teach"
+        v-model="editedUser.languageToTeach"
+        type="text"
+      />
       <label>Interests:</label>
       <div class="chooseInterests">
         <label
@@ -66,12 +81,20 @@
   main {
     margin-top: 30px;
   }
+  textarea {
+    width: 190px;
+    height: 100px;
+  }
 
   form {
     display: flex;
     align-items: center;
     justify-content: center;
     flex-direction: column;
+  }
+
+  .chooseInterests label {
+    margin: 10px;
   }
 
   .SaveButtonContainer {
